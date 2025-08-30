3e71e0940bc7ae473a27bd2cdf9181d68b1cc8f7
Himanshu116k <himanshu1991patna@gmail.com>
2025-08-30 17:58:04 +0530

    Add content

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -6,7 +6,7 @@ const HomeHeroText = () => {
     <div className="font-[font2] pt-5 text-center">
       <div className="text-[4.5vw] uppercase leading-[8vw] flex items-center justify-center" >Welcome</div>
       <div className="text-[4.5vw] uppercase leading-[8vw] flex items-center justify-center">Too</div>
-      <div className="text-[4.5vw] uppercase leading-[8vw] flex items-center justify-center flex-start ">B <div className="h-[8vw] w-[14vw] rounded-full overflow-hidden border-4 border-white "><Video/></div> ring</div>
+      <div className="text-[4.5vw] uppercase leading-[8vw] flex items-center justify-center flex-start ">Dh <div className="h-[8vw] w-[14vw] rounded-full overflow-hidden border-4 border-white "><Video/></div> ni</div>
       <div className="text-[4.5vw] uppercase leading-[8vw] flex items-center justify-center">Homepage</div>
     </div>
   );
