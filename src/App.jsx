afbab2e4dc0c631a4331a3a486501bef883caf14
Himanshu116k <himanshu1991patna@gmail.com>
2025-08-30 17:09:20 +0530

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
-import Project from './pages/project'
+import Project from './pages/Project'
 import Navbar from './components/Navigation/Navbar'
 
 
